30e9ed78f244ba705e31b673728731372cef7703
jihyeon <kth083030@naver.com>
2024-02-20 01:26:39 +0900

    대체문구추가

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 let newsList = []
 
 const getLatesNews = async ()=>{
-    const url = new URL(`https://jspractice03.netlify.app/top-headlines`);
+    const url = new URL(`https://jspractice03.netlify.app/top-headlines?pageSize=5`);
     const response = await fetch(url);
     const data = await response.json();
     newsList = data.articles;
@@ -19,20 +19,29 @@ getLatesNews();
 // news 랜더링
 
 const render=()=>{
-    const newsHTML = newsList.map(item=>` <div class="row news">
-    <div class="col-lg-4" style="margin-bottom: 10px;">
-        <img class="newsImage" src=${item.urlToImage} alt="img">
-    </div>
-    <div class="col-lg-8">
-        <h2>${item.title}</h2>
-        <p>${item.description}</p>
-        <div>
-            ${item.source.name} · ${item.publishedAt}
-        </div>
-    </div>
-</div>`).join('');
+    const newsHTML = newsList.map(item => {
 
-    console.log('html', newsHTML);
+        let imageUrl = item.urlToImage ? item.urlToImage : 'https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg'; // 이미지가 없는 경우 대체 이미지 사용
+        let desc = item.description ? (item.description.length > 200 ? item.description.substring(0, 200) + '...' : item.description) : '내용 없음';
+        let sc = item.source.name ? item.source.name : 'no source';
+
+        return `
+            <div class="row news">
+                <div class="col-lg-4" style="margin-bottom: 10px;">
+                    <img class="newsImage" src="${imageUrl}" alt="img">
+                </div>
+                <div class="col-lg-8">
+                    <h2>${item.title}</h2>
+                    <p>${desc}</p>
+                    <div>
+                        ${sc} ·  ${moment(
+                            item.publishedAt
+                         ).fromNow()}
+                    </div>
+                </div>
+            </div>
+        `;
+    }).join('');
 
     document.getElementById("newsBoard").innerHTML=newsHTML;
 }
