c539db83ff60a6a44ab7fc7084d8ca39962f5021
jihyeon <kth083030@naver.com>
2024-02-20 00:44:50 +0900

    뉴스렌더링

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
 
 //const API_KEY = 'my api key...'
 
-let news = []
+let newsList = []
 
 const getLatesNews = async ()=>{
     const url = new URL(`https://jspractice03.netlify.app/top-headlines`);
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("뉴스 : ",news);
+    newsList = data.articles;
+    render(); // newsList가 확정된 이후 render 호출
+    console.log("뉴스 : ",newsList);
 };
 
 getLatesNews();
+
+
+
+// news 랜더링
+
+const render=()=>{
+    const newsHTML = newsList.map(item=>` <div class="row news">
+    <div class="col-lg-4" style="margin-bottom: 10px;">
+        <img class="newsImage" src=${item.urlToImage} alt="img">
+    </div>
+    <div class="col-lg-8">
+        <h2>${item.title}</h2>
+        <p>${item.description}</p>
+        <div>
+            ${item.source.name} · ${item.publishedAt}
+        </div>
+    </div>
+</div>`).join('');
+
+    console.log('html', newsHTML);
+
+    document.getElementById("newsBoard").innerHTML=newsHTML;
+}
+
+
+
 
 
 
@@ -20,7 +47,6 @@ getLatesNews();
 // menu 
 const toggleBtn = document.querySelector('.navbar-togglebtn');
 const menu = document.querySelector('.menus');
-
 
 toggleBtn.addEventListener('click', () => {
 	
