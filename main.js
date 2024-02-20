ea151a905c91004a3708283a0ef48ee946bb4410
jihyeon <kth083030@naver.com>
2024-02-20 20:25:58 +0900

    카테고리/검색필터링

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 
-//const API_KEY = 'my key...'
-
+const API_KEY = 'my key..'
+// https://newsapi.org/v2/top-headlines?pageSize=3&country=kr&apiKey=${API_KEY}
+// https://jspractice03.netlify.app/top-headlines?pageSize=3
 let newsList = []
+const menus = document.querySelectorAll(".menus button")
+//console.log(menus)
+menus.forEach(menu=>menu.addEventListener("click",()=>getNewsByCategory(event)))
 
 const getLatesNews = async ()=>{
     const url = new URL(`https://jspractice03.netlify.app/top-headlines?pageSize=3`);
     const response = await fetch(url);
     const data = await response.json();
+    //console.log("test",data);
     newsList = data.articles;
     render(); // newsList가 확정된 이후 render 호출
     console.log("뉴스 : ",newsList);
@@ -15,9 +20,22 @@ const getLatesNews = async ()=>{
 getLatesNews();
 
 
+// 카테고리 
+const getNewsByCategory = async(event)=>{
+    const category = event.target.textContent.toLowerCase();
+    console.log("category",category);
+    const url = new URL(`https://jspractice03.netlify.app/top-headlines?category=${category}&pageSize=3`)
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log("data",data)
+
+    newsList = data.articles;
+    render();
+}
+
+
 
 // news 랜더링
-
 const render=()=>{
     const newsHTML = newsList.map(item => {
 
@@ -47,9 +65,9 @@ const render=()=>{
 }
 
 
-
-
-
+// 1. 카테고리 버튼 클릭 이벤트 추가
+// 2. 카테코리별 뉴스 가져오기(필터)
+// 3. 분류된 뉴스 보여주기(렌더)
 
 
 
@@ -63,7 +81,7 @@ toggleBtn.addEventListener('click', () => {
 	
 });
 
-// search
+// 검색 on/off
 const openSearch = () => {
     let inputArea = document.getElementById("inputArea");
     if (inputArea.style.display === "inline") {
@@ -72,3 +90,17 @@ const openSearch = () => {
       inputArea.style.display = "inline";
     }
   };
+
+// 검색 기능
+const searchNews = async()=>{
+    const keyword = document.getElementById("searchInput").value;
+    console.log("키워드",keyword);
+    const url = new URL(`https://jspractice03.netlify.app/top-headlines?country=kr&q=${keyword}`)
+    const response = await fetch(url) // url 부른다
+    const data = await response.json() // json 형태로 뽑는다
+    console.log("키워드 data",data)
+    
+    newsList = data.articles; // 검색결과를 다시 리스트에 담기 
+
+    render()  // 렌더링
+}
